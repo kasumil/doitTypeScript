0a8d1121da9ec9f319d6f2dc820371ea744e8665
kasumil <ridou99@gmail.com>
2020-11-10 03:49:46 +0900

    프로젝트 끝

---
+++ ch12-3/src/csv/csvFileReaderGenerator.ts
@@ -0,0 +1,12 @@
+import {readFileGenerator} from "../fileApi/readFileGenerator"
+import {zip} from "../utils/zip"
+
+export function* csvFileReaderGenerator(filename: string, delim: string=',') {
+  let header = []
+  for(let line of readFileGenerator(filename)) {
+    if(!header.length)
+      header = line.split(delim)
+    else
+      yield zip(header, line.split(delim))
+  }
+}
